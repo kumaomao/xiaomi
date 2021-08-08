78df9a567c9b9a2f68403ecc5dfa26651be8f5c2
kumaomao <1083271136@qq.com>
2021-08-08 19:44:30 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module xiaomi
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"xiaomi/xiaomi"
+)
+
+func main()  {
+	http.Handle("/file/",  http.StripPrefix("/file/", http.FileServer(http.Dir("./template/file"))))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/run", run)
+
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		fmt.Println("HTTP server failed,err:", err)
+		return
+	}
+}
+
+func run(w http.ResponseWriter, r *http.Request)  {
+	r.ParseForm()
+
+	username := r.Form.Get("user")
+	password := r.Form.Get("password")
+	step := r.Form.Get("step")
+
+	token,err := xiaomi.Login(username,password)
+	if err != nil{
+		fmt.Fprintf(w, `{"code":0,"message":"登录失败"}`)
+		return
+	}
+	//int, _ := strconv.Atoi(step)
+	res,_ := xiaomi.Run(token,step)
+	if res{
+		fmt.Fprintf(w, `{"code":0,"message":"步数已修改为`+step+`步"}`)
+		return
+	}
+	fmt.Fprintf(w, `{"code":0,"message":"刷取失败"}`)
+
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("./template/index.html")
+	if err != nil {
+		fmt.Println("create template failed, err:", err)
+		return
+	}
+	query := r.URL.Query()
+	data := map[string]string{
+		"id":  query.Get("id"),
+		"url":query.Get("url"),
+	}
+	tmpl.Execute(w, data)
+}
